29af035d68a3b7adaaa9abc88c8de90652574f19
Dev Govindji <friendsofdev94@gmail.com>
2019-06-24 18:35:00 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -39,34 +39,54 @@ var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!");
+			alert("You found a match!");
 		} else {
-			console.log("Sorry, try again.");
+			alert("Sorry, try again.");
 		}
 };
 
 
 
-function flipCard(cardId) {
+function flipCard() {
+	
+	const cardId = this.getAttribute("data-id");//good
+	
+	this.setAttribute("src", cards[cardId].cardImage);//issue
 
 	if (cardsInPlay.length === 2) {
 	checkForMatch();	
 	} console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage); //issue
-	console.log(cards[cardId].suit); //issue
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 };
 
-flipCard(0);
-flipCard(2);
 
 
 
 
+//issue
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) { //good
+		
+
+		var cardElement = document.createElement('img'); //good
+		cardElement.setAttribute("src", "images/back.png"); //good
+		cardElement.setAttribute("data-id", i);//good
+		cardElement.addEventListener("click", flipCard); //good
+		document.getElementById("game-board").appendChild(cardElement); //good
+	}
+
+};
 
 
 
+createBoard();
 
+//should reset after getting match
+//should alert before it flips
+
+//issue
 
 
 
